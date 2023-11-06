feac4d8f304c82683125aa4a46943391c4b19502
Alexander Lebedev <lebastr@gmail.com>
2023-11-06 17:28:04 +0300

    *

--- flake.nix
+++ flake.nix
@@ -1,16 +1,6 @@
 {
   description = "Isolated workspaces are implemented through a NixOS container";
-
-  inputs = {
-    home-manager.url = "github:nix-community/home-manager";
-    nixpkgs.url = "github:/NixOS/nixpkgs";
-  };
-
   outputs = { self, nixpkgs, home-manager }: {
-    nixosModules.workspaces = { config, nix-doom-emacs }: import ./container-workspace.nix { config = config;
-                                                                             lib = nixpkgs.lib;
-                                                                             pkgs = nixpkgs;
-                                                                             home-manager = home-manager;
-                                                                           };
+    nixosModules.workspaces = import ./container-workspace.nix;
   };
 }
