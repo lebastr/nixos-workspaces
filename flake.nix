25d61b9c487b6accff486a577ef98b2a43cbbb57
Alexander Lebedev <lebastr@gmail.com>
2023-11-06 15:23:27 +0300

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Isolated workspaces are implemented through a NixOS container";
+
+  inputs = {
+    home-manager.url = "github:nix-community/home-manager";
+    nixpkgs.url = "github:/NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
+    nixosModules.workspaces = { config }: import ./container-workspace.nix { config = config;
+                                                                             lib = nixpkgs.lib;
+                                                                             pkgs = nixpkgs;
+                                                                             home-manager = home-manager;
+                                                                           };
+  };
+}
